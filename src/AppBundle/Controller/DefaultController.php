17a708e85a02f22f1fa1d88e9ffc571c871b9f1b
Symbolic76 <damien.gibert76@gmail.com>
2018-06-01 16:38:17 +0200

    Damien

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -27,4 +27,29 @@ class DefaultController extends Controller
     public function deleteAction(){
         return $this->render('@App/Default/delete.html.twig');
     }
+    public function dashboardAction(){
+        return $this->render('@App/Default/dashboard.html.twig');
+    }
+    public function widgetsAction(){
+        return $this->render('@App/Default/widgets.html.twig');
+    }
+    public function chartsAction(){
+        return $this->render('@App/Default/charts.html.twig');
+    }
+    public function tablesAction(){
+        return $this->render('@App/Default/tables.html.twig');
+    }
+    public function formsAction(){
+        return $this->render('@App/Default/forms.html.twig');
+    }
+    public function panelsAction(){
+        return $this->render('@App/Default/panels.html.twig');
+    }
+    public function iconsAction(){
+        return $this->render('@App/Default/icons.html.twig');
+    }
+    public function loginAction(){
+        return $this->render('@App/Default/login.html.twig');
+    }
+    
 }
